ad5c2c69fce2bbf191e07ef860830aa188ce1596
Mariano Jeb <gabriel.marcheti.mariano@gmail.com>
2026-01-30 15:19:19 -0300

    Yield

---
+++ Aleatoriedades/generators.py
@@ -0,0 +1,15 @@
+def gen_primes(inicio:int, fim:int)->int:
+    for num in range (inicio,fim+1):
+        if num<2:
+            continue
+        prime=True
+        for div in range(2,num):
+            if num%div==0:
+                prime=False
+                break
+        if prime:
+            yield num
+
+numerozitos=gen_primes(50,100)
+for i in numerozitos:
+    print(i)
